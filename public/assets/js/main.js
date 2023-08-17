e5688f4517dcd210f7d9efac22c1c8ceb4f6f070
Vishva <vishva.dave283@gmail.com>
2023-08-17 18:53:57 +1200

    Created a function to use the data from the JSON file containing the list of Cells and display it visually on the calendar

---
+++ public/assets/js/main.js
@@ -0,0 +1,16 @@
+function onDisplay(jsonData) {
+    // Loop through each Cell object in the JSON data
+    for (const cell of jsonData) {
+        // Calculate the opacity based on the ratio of numPeople and users array length
+        const opacity = cell.numPeople === 0 ? 0 : cell.users.length / cell.numPeople;
+        
+        // Find the corresponding table cell in the HTML
+        const cellElement = document.getElementById(cellId);
+        
+        // Apply the calculated background color and opacity to the cell if found
+        if (cellElement) {
+            // Set background color with dynamic opacity using rgba
+            cellElement.style.backgroundColor = "rgba(128, 0, 0, " + opacity + ")";  // setting colour to red with specified opacity
+        }
+    }
+}
