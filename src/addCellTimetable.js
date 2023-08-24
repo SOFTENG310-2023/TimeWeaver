7921b4188b8255a8c05350ea6f3935267974a116
MRlolface249 <ahop089@aucklanduni.ac.nz>
2023-08-25 00:56:34 +1200

    removed unnecessary commenting as requested by luca

--- src/addCellTimetable.js
+++ src/addCellTimetable.js
@@ -1,11 +1,10 @@
-// function calls calls method to create a cell object then adds that object to the inputted array obj
+// function calls method to create a cell object then adds that object to the inputted array obj
 function addCellTimetable(id, user, userCustomObj) {
-  const cell = createCellInstance(id, user, 1); // creates cell instance
+  const cell = createCellInstance(id, user, 1);
   userCustomObj.cells.push(cell); // adds cell to inputted array
   return userCustomObj;
 }
 
-// function creates an instance of a cell object and then returns that instance
 function createCellInstance(id, user, numPeople) {
   return {
     ID: id,
