c2ea408e2161c2a84ae03cc893d33c5305e155f3
vdav02 <100258989+vdav02@users.noreply.github.com>
2023-08-25 03:03:46 +1200

    Display the JSON file format visually on the calendar (#28)
    * Created a function to use the data from the JSON file containing the list of Cells and display it visually on the calendar
    
    * Created test file and made slight fixes to onDisplay()
    
    * Added additional tests to the test suite for display.js
    
    * Added comments documenting bug as per PR review request
    
    * Implemented json data entry changes as requested in pull review request
    
    * pull request changes made

---
+++ src/onDisplay.js
@@ -0,0 +1,19 @@
+const { getNumberOfCalendars } = require("./manageCalendars");
+
+function onDisplay(jsonData) {
+  // Loop through each Cell object in the JSON data
+  for (const cell of jsonData.cells) {
+    // Calculate the opacity based on the ratio of numPeople and users array length
+    const opacity =
+      cell.numPeople === 0 ? 0 : cell.numPeople / getNumberOfCalendars();
+
+    // Find the corresponding table cell in the HTML
+    const cellElement = document.getElementById(cell.id);
+
+    // Apply the calculated background color and opacity to the cell if found
+    if (cellElement) {
+      // Set background color with dynamic opacity using rgba
+      cellElement.style.backgroundColor = "rgba(128, 0, 0, " + opacity + ")"; // setting colour to red with specified opacity
+    }
+  }
+}
---
+++ src/tests/onDisplay.test.js
@@ -0,0 +1,89 @@
+// Note that the test suite is currently being skipped using jest due to a bug present in referencing jQuery and Fomantic UI JS dependencies. As soon as this bug is fixed, tests will be running automatically with jest.
+const onDisplay = require("../onDisplay");
+
+// Mocking the entire getNumberOfCalendars function
+jest.mock("../manageCalendars", () => ({
+  getNumberOfCalendars: jest.fn(() => 5), // Mock the function to always return 5
+}));
+
+describe.skip("onDisplay.js test suite", () => {
+  test("onDisplay applies correct background color and opacity", () => {
+    const jsonData = {
+      cells: [
+        {
+          id: "mon-0730",
+          users: ["Alex"],
+          numPeople: 1,
+        },
+      ],
+    };
+
+    // Run the onDisplay function with the cell
+    onDisplay(jsonData);
+
+    // Assert that the background color and opacity are correctly applied
+    const mockCellElement = document.getElementById("mon-0730");
+    expect(mockCellElement.style.backgroundColor).toBe("rgba(128, 0, 0, 0.2)");
+  });
+
+  test("onDisplay applies correct background color and opacity for multiple cells", () => {
+    const jsonData = {
+      cells: [
+        {
+          id: "mon-0730",
+          users: ["Alex"],
+          numPeople: 1,
+        },
+        {
+          id: "tue-0930",
+          users: ["Alice", "Bob"],
+          numPeople: 2,
+        },
+      ],
+    };
+
+    onDisplay(jsonData);
+
+    const mockCellElement1 = document.getElementById("mon-0730");
+    expect(mockCellElement1.style.backgroundColor).toBe("rgba(128, 0, 0, 0.2)");
+
+    const mockCellElement2 = document.getElementById("tue-0930");
+    expect(mockCellElement2.style.backgroundColor).toBe("rgba(128, 0, 0, 0.4)");
+  });
+
+  test("onDisplay applies zero opacity when numPeople is 0", () => {
+    const jsonData = {
+      cells: [
+        {
+          id: "wed-1100",
+          users: [],
+          numPeople: 0,
+        },
+      ],
+    };
+
+    onDisplay(jsonData);
+
+    const mockCellElement = document.getElementById("wed-1100");
+    expect(mockCellElement.style.backgroundColor).toBe("rgba(128, 0, 0, 0)");
+  });
+
+  test("onDisplay handles missing cell elements gracefully", () => {
+    const jsonData = {
+      cells: [
+        {
+          id: "thu-1500",
+          users: ["Eva"],
+          numPeople: 1,
+        },
+      ],
+    };
+
+    // Mocking a scenario where getElementById returns null
+    document.getElementById = jest.fn((id) => null);
+
+    onDisplay(jsonData);
+
+    // No assertions needed, just making sure the function does not throw errors
+  });
+});
