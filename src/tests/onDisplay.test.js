4411ea32a37f7da0cdf4872007ad4d98210a44f3
vdav02 <100258989+vdav02@users.noreply.github.com>
2023-08-24 18:43:06 +1200

    Created test file and made slight fixes to onDisplay()

---
+++ src/tests/onDisplay.test.js
@@ -0,0 +1,7 @@
+const onDisplay = require("../onePlusTwo");
+
+describe("onDisplay.js function test suite", () => {
+  test("Function should return 3", async () => {
+    expect(onePlusTwo()).toBe(3);
+  });
+});
