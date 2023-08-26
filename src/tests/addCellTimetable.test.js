b579b180edde017c8d01ce46ef8f29b57b62eb5e
Kenny Lam <lamkenny1010@gmail.com>
2023-08-27 02:53:51 +1200

    Rewrote addCellTimetable test case.

--- src/tests/addCellTimetable.test.js
+++ src/tests/addCellTimetable.test.js
@@ -1,56 +1,18 @@
-const addCellTimetable = require("../addCellTimetable");
+const createCellInstance = require("../addCellTimetable");
 
-describe.skip("Unit testing for addCellTimetable function", () => {
-  test("Function should add a cell to an empty cells array", async () => {
-    const inputUserCustomObj = {
-      cells: [],
-    };
-    const user = "Alex";
-    const id = "mon-0800";
+describe("Unit testing for addCellTimetable function", () => {
+    test("Function should add a cell to an empty cells array", () => {
+        const user = "Alex";
+        const id = "mon-0800";
+        const numPeople = 1;
 
-    const expectedOutputUserCustomObj = {
-      cells: [
-        {
-          ID: "mon-0800",
-          users: ["Alex"],
-          numPeople: 1,
-        },
-      ],
-    };
-
-    expect(addCellTimetable(id, user, inputUserCustomObj)).toEqual(
-      expectedOutputUserCustomObj,
-    );
-  });
-  test("Function should add a cell to an already existing cell array", async () => {
-    const inputUserCustomObj = {
-      cells: [
-        {
-          ID: "mon-0800",
-          users: ["Alex"],
-          numPeople: 1,
-        },
-      ],
-    };
-    const user = "Alex";
-    const id = "mon-0830";
-    const expectedOutputUserCustomObj = {
-      cells: [
-        {
-          ID: "mon-0800",
-          users: ["Alex"],
-          numPeople: 1,
-        },
-        {
-          ID: "mon-0830",
-          users: ["Alex"],
-          numPeople: 1,
-        },
-      ],
-    };
-
-    expect(addCellTimetable(id, user, inputUserCustomObj)).toEqual(
-      expectedOutputUserCustomObj,
-    );
-  });
+        const expectedOutputUserCustomObj = {
+            ID: "mon-0800",
+            users: ["Alex"],
+            numPeople: 1,
+        };
+        expect(createCellInstance(id, user, numPeople)).toEqual(
+            expectedOutputUserCustomObj
+        );
+    });
 });
