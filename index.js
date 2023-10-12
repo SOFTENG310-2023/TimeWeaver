37d878ed61dd29d9a8b6df13600b056ff908756b
Raymond <65014987+rl16432@users.noreply.github.com>
2023-10-12 19:23:20 +1300

    Setup for database using Supabase (#74)
    * refactor express backend to separate routers
    
    * setup supabase

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const fetch = require("node-fetch");
 // Query parser
 const qs = require("qs");
 
-// A very simple express application that just serves static files
 const express = require("express");
+const getIcalRouter = require("./src/api/routers/get-ical.js");
+const userRouter = require("./src/api/routers/user.js");
 const app = express();
 
 // Defaults to port 8080 in development
@@ -13,31 +14,13 @@ const port = process.env.port || 8080;
 
 // Routes all files in public relative to the root url
 app.use(express.static("public"));
+app.use("/api/get-ical", getIcalRouter);
+app.use("/api/user", userRouter);
 
 app.set("query parser", (str) => {
   return qs.parse(str, {
     /* custom options */
   });
-});
-
-// Sets up the proxy
-app.get("/api/get-ical", async (req, res) => {
-  const icalUrl = req.query.ical;
-
-  try {
-    const icalUrlRes = await fetch(icalUrl);
-    if (!icalUrlRes.ok) {
-      throw new Error("Failed to fetch iCalendar data");
-    }
-
-    const icalContent = await icalUrlRes.text();
-    res.set("Content-Type", "plain/text");
-    res.send(icalContent);
-  } catch (error) {
-    res.status(500).json({
-      error: "An error occurred while fetching the iCalendar data.",
-    });
-  }
 });
 
 const server = app.listen(port, () => {
