f51e402d54cf9e590325500df6b728b7447acf14
rl16432 <rluo52237@gmail.com>
2023-10-17 21:13:53 +1300

    test: DTO mapping to app models

---
+++ src/tests/dtoMapping.test.js
@@ -0,0 +1,12 @@
+const {
+  selectedSlotConverter,
+} = require("../helpers/dto_mapping/calendarMapping");
+describe("Unit tests for dtoMapping function", () => {
+  test("Time slot converter 1", () => {
+    const slot = {
+      day: "Monday",
+      timeslot: "07:30:00",
+    };
+    expect(selectedSlotConverter(slot)).toBe("mon-0730");
+  });
+});
