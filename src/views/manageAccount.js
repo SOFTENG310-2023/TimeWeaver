db1ca68e8710afd7ca5ecedf904ade6f99317e93
Dana <mseo447@aucklanduni.ac.nz>
2023-10-19 20:42:20 +1300

    Finished user auth (redo because forgot to fork)

--- src/views/manageAccount.js
+++ src/views/manageAccount.js
@@ -1,24 +1,28 @@
-const { accountModal, signupModal } = require("./modals");
+const { loginModal, signupModal, accountModal } = require("./modals");
+const config = require("../utils/config");
+const { createClient } = require("@supabase/supabase-js");
 
 /** HTML Element Declarations */
-const openAccountBtn = document.getElementById("user-account-button");
-const usernameInput = document.getElementById("login-username-input");
-const passwordInput = document.getElementById("login-password-input");
+const openLoginBtn = document.getElementById("user-account-button");
+const openAccountBtn = document.getElementById("view-account-button");
 const loginBtn = document.getElementById("login-btn");
+const logoutBtn = document.getElementById("logout-btn");
 const miniLoginBtn = document.getElementById("mini-login-btn");
 const signupBtn = document.getElementById("signup-btn");
 const miniSignupBtn = document.getElementById("mini-signup-btn");
 
 /** Mapping buttons to their onClick functions */
+openLoginBtn.addEventListener("click", openLoginPopup);
 openAccountBtn.addEventListener("click", openAccountPopup);
 miniSignupBtn.addEventListener("click", openSignupPopup);
-miniLoginBtn.addEventListener("click", openAccountPopup);
+miniLoginBtn.addEventListener("click", openLoginPopup);
 loginBtn.addEventListener("click", userLogin);
 signupBtn.addEventListener("click", userSignup);
+logoutBtn.addEventListener("click", userLogout);
 
-function openAccountPopup() {
+function openLoginPopup() {
   // Open popup
-  accountModal.modal("show");
+  loginModal.modal("show");
 
   // Clear input fields
   document.getElementById("login-email-input").value = "";
@@ -35,19 +39,126 @@ function openSignupPopup() {
   document.getElementById("signup-password-input").value = "";
 }
 
+function openAccountPopup() {
+  accountModal.modal("show");
+}
+
 function userLogin() {
-  // TO-DO: implement user login functionality
-  alert("Implement user login functionality");
+  const userData = {
+    email: document.getElementById("login-email-input").value,
+    password: document.getElementById("login-password-input").value,
+  };
+
+  const authRes = fetch("api/user/login", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(userData),
+  })
+    .then((res) => {
+      if (res.status == 200) {
+        loginModal.modal("hide");
+        alert("Login Successful.");
+      } else {
+        alert("Oops, something went wrong. Please try again.");
+      }
+
+      return res.json();
+    })
+    .then((data) => {
+      localStorage.setItem("session", data.session.access_token);
+      localStorage.setItem("user_id", data.user.id);
+
+      handleLogin();
+    });
 }
 
 function userSignup() {
-  // TO-DO: implement user sign up functionality
-  alert("Implement user sign up functionality");
+  const userData = {
+    email: document.getElementById("signup-email-input").value,
+    password: document.getElementById("signup-password-input").value,
+    name: document.getElementById("signup-name-input").value,
+  };
+
+  const authRes = fetch("/api/user/create-account", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(userData),
+  })
+    .then((res) => {
+      if (res.status == 200) {
+        signupModal.modal("hide");
+        alert("Account successfully created.");
+      } else {
+        alert("Oops, something went wrong. Please try again.");
+      }
+
+      return res.json();
+    })
+    .then((data) => {
+      // Make a POST request to enter user information into the databasea
+      console.log(data);
+      const sendData = {
+        id: data.user.id,
+        name: document.getElementById("signup-name-input").value,
+      };
+
+      fetch("/api/user/after-creation", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(sendData),
+      }).then(() => {
+        localStorage.setItem("session", data.session.access_token);
+        localStorage.setItem("user_id", data.user.id);
+
+        handleLogin();
+      });
+    });
+}
+
+function userLogout() {
+  // Remove user info from the local storage
+  localStorage.removeItem("session");
+  localStorage.removeItem("user_id");
+
+  handleLogin();
+  accountModal.modal("hide");
+}
+
+function handleLogin() {
+  const session = localStorage.getItem("session");
+
+  if (session === null) {
+    // user is not logged in
+    document.getElementById("view-account-button").style.display = "none";
+    document.getElementById("user-account-button").style.display = "flex";
+  } else {
+    document.getElementById("view-account-button").style.display = "flex";
+    document.getElementById("user-account-button").style.display = "none";
+
+    const token = localStorage.getItem("session");
+
+    fetch(`/api/user/${token}`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // Update info on the my account modal
+        document.getElementById(
+          "name-info"
+        ).textContent = `Name: ${data.user.user_metadata.name}`;
+        document.getElementById(
+          "email-info"
+        ).textContent = `Email: ${data.user.email}`;
+      });
+  }
 }
 
 module.exports = {
-  openAccountPopup,
-  openSignupPopup,
-  userLogin,
-  userSignup,
+  handleLogin,
 };
